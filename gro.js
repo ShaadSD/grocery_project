194104abcf87c5d676d2713e5c445d587d4b88b1
unknown <you@example.com>
2024-11-25 23:20:40 -0800

    card done

---
+++ gro.js
@@ -0,0 +1,37 @@
+// const loadServices = () => {
+//     fetch("http://127.0.0.1:8000/product_list/")
+//         .then((res) => res.json())
+//         .then((data) => displayService(data))
+//         .catch((err) => console.log(err));
+// };
+
+// const displayService = (services) => {
+//     console.log(services);
+//     services.forEach((service) => {
+//         const parent = document.getElementById("card-2");
+//         const div = document.createElement("div");
+//         div.innerHTML = `
+//         <div>
+           
+//         </div>
+//         `;
+//         parent.appendChild(div);
+        
+//     });
+// };
+// loadServices();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
