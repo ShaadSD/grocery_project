f5ecd515c2b2aa44c5f1b155b71e1ab3e3b5d6c8
unknown <you@example.com>
2024-11-27 04:07:13 -0800

    Added new feature

--- gro.js
+++ gro.js
@@ -1,37 +1,29 @@
-// const loadServices = () => {
-//     fetch("http://127.0.0.1:8000/product_list/")
-//         .then((res) => res.json())
-//         .then((data) => displayService(data))
-//         .catch((err) => console.log(err));
-// };
+const loadServices1 = () => {
+    fetch("http://127.0.0.1:8000/product_list/")
+        .then((res) => res.json())
+        .then((data) => displayService(data));
+};
 
-// const displayService = (services) => {
-//     console.log(services);
-//     services.forEach((service) => {
-//         const parent = document.getElementById("card-2");
-//         const div = document.createElement("div");
-//         div.innerHTML = `
-//         <div>
-           
-//         </div>
-//         `;
-//         parent.appendChild(div);
-        
-//     });
-// };
-// loadServices();
+const displayService = (services) => {
+    console.log(services);
+    const parent = document.getElementById("card-container");
 
-
-
-
-
+    services.forEach((service) => {
+        const div = document.createElement("div");
+        div.className = "col"; // Bootstrap column
+        div.innerHTML = `
+            <div class="card">
+                <img src="${service.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${service.title}</h5>
+                    <p class="card-text">${service.description}</p>
+                </div>
+            </div>
+        `;
+        parent.appendChild(div);
+    });
+};
 
 
 
-
-
-
-
-
-
-
+loadServices1();
